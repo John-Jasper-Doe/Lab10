b6a6de0d55cc62757ad6824da806b70e144f7ae7
John Doe <john.jasper.doe@gmail.com>
2020-06-29 13:50:03 +0300

    Initial commit

---
+++ src/bulkmt.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file bulkmt.cpp
+ * @brief Main file in this project.
+ *
+ * @author Maxim <john.jasper.doe@gmail.com>
+ * @date 2020
+ */
+
+/* See the license in the file "LICENSE.txt" in the root directory. */
+
+#include "version.hpp"
+#include <iostream>
+
+
+/** @brief Main entry point */
+int main(int argc, char* argv[]) {
+  std::cout << "BULKMT v." << BULKMT_MAJOR << "." << BULKMT_MINOR << "." << BULKMT_PATCH << "\n";
+  std::cout << "  Make by Maxim <john.jasper.doe@gmail.com>" << std::endl;
+
+  (void) argc;
+  (void) argv;
+}
