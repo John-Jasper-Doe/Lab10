d3108ae57aaa4b292f77d6ea87cdf93e2dac784c
John Doe <john.jasper.doe@gmail.com>
2020-07-05 22:14:25 +0300

    Added of the 'console logger' and of the 'file logger' classes

---
+++ src/io/conlogger.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file conlogger.cpp
+ * @brief Implementation of the "Console Logger" class.
+ *
+ * @author Maxim <john.jasper.doe@gmail.com>
+ * @date 2020
+ */
+
+#include "conlogger.hpp"
+
+/** @brief The namespace of the BULKMT project */
+namespace bulkmt {
+/** @brief The namespace of the IO */
+namespace io {
+
+conlogger::conlogger(std::ostream& ostrm, const std::string& str,
+                     const common::counter& cnt) noexcept
+  : ostrm_(ostrm), str_(std::move(str)), cnt_(cnt) {}
+
+common::counter conlogger::start() noexcept {
+  ostrm_ << str_ << '\n';
+  return cnt_;
+}
+
+} /* io:: */
+} /* bulkmt:: */
